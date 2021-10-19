0bfb148b5bba474f1e96c46399d78f92dfca43a6
unknown <21614306@qq.com>
2021-10-19 17:21:05 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use  App\Http\Controllers\ArticleController;
 |
 */
 
-Route::get('/123', [ArticleController::class, 'index'] );
+Route::get('/', [ArticleController::class, 'index'] );
 
-Route::get('/', [ArticleController::class, 'solutions'] );
+Route::get('/solutions', [ArticleController::class, 'solutions'] );
 Route::get('/product-show', [ArticleController::class, 'productShow'] );
 Route::get('/customer-case', [ArticleController::class, 'customerCase'] );
 Route::get('/service-center', [ArticleController::class, 'serviceCenter'] );
@@ -27,21 +27,21 @@ Route::get('/about-us', [ArticleController::class, 'about'] );
 
 
 
-Route::get('/authDetail', [ArticleController::class, 'authDetail'] );
-Route::post('/authSearch', [ArticleController::class, 'authSearch'] );
-Route::get('/articleDetail', [ArticleController::class, 'articleDetail'] );
+// Route::get('/authDetail', [ArticleController::class, 'authDetail'] );
+// Route::post('/authSearch', [ArticleController::class, 'authSearch'] );
+// Route::get('/articleDetail', [ArticleController::class, 'articleDetail'] );
 
 
 
 
-Route::get('/tixi', [ArticleController::class, 'tixi'] );
-Route::get('/grs', [ArticleController::class, 'grs'] );
-Route::get('/success', [ArticleController::class, 'success'] );
+// Route::get('/tixi', [ArticleController::class, 'tixi'] );
+// Route::get('/grs', [ArticleController::class, 'grs'] );
+// Route::get('/success', [ArticleController::class, 'success'] );
 
-Route::get('/news', [ArticleController::class, 'news'] );
+// Route::get('/news', [ArticleController::class, 'news'] );
 
-Route::get('/abouts', [ArticleController::class, 'abouts'] );
-Route::get('/liucheng', [ArticleController::class, 'liucheng'] );
+// Route::get('/abouts', [ArticleController::class, 'abouts'] );
+// Route::get('/liucheng', [ArticleController::class, 'liucheng'] );
 
 
 
